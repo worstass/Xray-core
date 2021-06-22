580c2a0a526e7b31aa6580126c97f3089e31216a
nobody <nobody@nowhere.com>
2021-06-22 16:42:39 +0800

    extra config changed

--- infra/conf/extra.go
+++ infra/conf/extra.go
@@ -9,19 +9,10 @@ type ExtraConfig struct {
 	Domain string `json:"domain"`
 	Email  string `json:"email"`
 	FsRoot string `json:"fsRoot"`
-	Spine  *struct {
-		Host string `json:"host"`
-		Port int32  `json:"port"`
-	} `json:"spine"`
-	Consul *struct {
-		Host string `json:"host"`
-		Port int32  `json:"port"`
-	} `json:"consul"`
-	Prometheus *struct {
-		Host      string `json:"host"`
-		Port      int32  `json:"port"`
-		LocalPort int32  `json:"localPort"`
-	} `json:"prometheus"`
+	SpineAddress string `json:"spineAddress"`
+	ConsulAddress string `json:"consulAddress"`
+	PromExporterAddress string `json:"promExporterAddress"`
+	AuthUseSpine bool `json:"authUseSpine"`
 }
 
 func (c *ExtraConfig) Build() (proto.Message, error) {
@@ -29,18 +20,17 @@ func (c *ExtraConfig) Build() (proto.Message, error) {
 		Domain: c.Domain,
 		Email:  c.Email,
 		FsRoot: c.FsRoot,
-		Spine: &extra.Spine{
-			Host: c.Spine.Host,
-			Port: c.Spine.Port,
-		},
-		Consul: &extra.Consul{
-			Host: c.Consul.Host,
-			Port: c.Consul.Port,
-		},
-		Prometheus: &extra.Prometheus{
-			Host:      c.Prometheus.Host,
-			Port:      c.Prometheus.Port,
-			LocalPort: c.Prometheus.LocalPort,
-		},
+		SpineAddress: c.SpineAddress,
+		ConsulAddress: c.ConsulAddress,
+		//Spine: &extra.Spine{
+		//	Host: c.Spine.Host,
+		//	Port: c.Spine.Port,
+		//},
+		//Consul: &extra.Consul{
+		//	Host: c.Consul.Host,
+		//	Port: c.Consul.Port,
+		//},
+		PromExporterAddress: c.PromExporterAddress,
+		AuthzUseSpine: c.AuthUseSpine,
 	}, nil
 }
